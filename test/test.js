accbe9b039672cd5436de87780ca3c8e146763e1
Jesus Antonio Perez sandoval <jasp402@gmail.com>
2019-05-07 17:03:45 -0500

    fix: update test

--- test/test.js
+++ test/test.js
@@ -8,10 +8,15 @@ describe('TEST # 1 - Test of integrity all functions', function () {
         assert.ok(typeof u.differenceDay === 'function');
     });
 
-    it('should, Current Date in format string',function () {
-        console.log(u.customDate());
-        assert.ok(typeof u.customDate() === 'string');
-    })
+    it('Test, function customDate()',function () {
+        assert.equal(u.customDate('05/07/2019', 'yyyy-mm-dd'), '2019-05-07');
+        assert.equal(u.customDate('05/07/2019', 'yyyy-mm-dd', 10, true), new Date('05/17/2019  12:00:00'));
+    });
+
+    it('Test, function differenceDay()',function () {
+        assert.equal(u.differenceDay('01/01/2019', '01/17/2019'), '16'); //count since
+        assert.equal(u.differenceDay('05/01/2019', '01/17/2019'), '-104');
+    });
 });
 
 
