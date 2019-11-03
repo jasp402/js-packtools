e0c8f7075bd8e73815f64bfe5dfed8fa0e5ab98c
jasp402 <jasp402@gmail.com>
2019-11-03 08:24:30 -0500

    feat: writeLogError add throw Exception. update test for WriteLogError

--- test/test_writeLogError.js
+++ test/test_writeLogError.js
@@ -3,18 +3,11 @@ const jsPackTools = require('../index');
 let u = new jsPackTools();
 
 describe('SUITE - logError()', function () {
-    it('TEST # 1, return  new document', function () {
-        // Logic: return new file
-        u.writeLogError('test # 1');
-       
+    it('TEST # 1, Create throw exception', function () {
+        try {
+            u.writeLogError('Title Error', 'Message of error with details');
+        }catch (e) {
+            console.log(e);
+        }
     });
-    it('TEST #2 , create file  simple',function(){
-        //logic: create a  file
-        setTimeout(function () {
-            u.writeLogError('Test #2');
-        }, 3500);
-
-    })
-
-    
 });
