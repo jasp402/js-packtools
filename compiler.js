4654bf0405ebd4bb942a28278ae59e090f8bc23f
jasp402 <jasp402@gmail.com>
2019-11-02 23:09:32 -0500

    feat: compiler automatically generates the index.js file

--- compiler.js
+++ compiler.js
@@ -3,14 +3,36 @@
  * @returns {void}
  * @since 1.0.0
  */
-const fs       = require('fs');
-
+var pjson  = require('./package.json');
+const fs   = require('fs');
+let header = (version, date)=>'/**\n' +
+    ' * JS-PackTools. Is a little toolBox for developer specifically with webScraping and format date and text\n' +
+    ' * @module JS-PackTools | API Doc\n' +
+    ' * @author Jesús A. Pérez S.\n' +
+    ' * {@version '+version+' | last Update: '+date+' \n' +
+    ' * {@link https://github.com/jasp402/js-packtools}\n' +
+    ' */\n' +
+    'const constant \t\t\t = require(__dirname+\'/constants\');\n' +
+    'const assert \t\t\t = require(\'assert\');\n';
+let params     = 'class parameters {\n' +
+    '    constructor(config={}){\n' +
+    '        assert.ok(typeof config === \'object\', constant.ERROR_CONSTRUCTOR);\n' +
+    '        this.constant           = constant;\n' +
+    '        this.folderName         = ("folderName"        in config) ? config.folderName               : \'DATA\';               //=> data_audit, data_download, docs, pdfs, trash...\n' +
+    '        this.folderWithDate     = ("folderAutoDate"    in config) ? config.folderWithDate           : true;                 //=> true => /2019_01_01/\n' +
+    '        this.folderFormatDate   = ("folderFormatDate"  in config) ? config.folderFormatDate         : \'yyyy~_~mm~_~dd\';     //=> \'yyyy_mm_dd\';\n' +
+    '        this.nameWriteLog       = ("nameWriteLog"      in config) ? config.nameWriteLog+\'.log\'      : \'logExecution.log\';\n' +
+    '        this.nameWriteLogError  = ("nameWriteLogError" in config) ? config.nameWriteLogError+\'.log\' : \'logError.log\';\n' +
+    '        this.logFormatDate      = ("logFormatDate"     in config) ? config.logFormatDate            : \'yyyy-mm-dd ~h:m:i\';  //=> yyyy-mm-dd | hh:mm:sss\n' +
+    '    }\n' +
+    '}\n';
 let classBuild = `class jsPackTools extends parameters { \n`;
 let importMod  = '';
 let arMethods  = [];
 fs.readdirSync(__dirname+'/lib').forEach(modules => {
     let module   = modules.split('.').shift();
-    importMod   += `const ${module} = require('./lib/${module}');\n`;
+    let space    = module.length <= 4 ? "\t\t\t\t" : module.length <= 8 ? "\t\t\t" : module.length < 13 ? "\t\t" : "\t";
+    importMod   += `const ${module} ${space} = require('./lib/${module}');\n`;
     classBuild  += `get ${module}() { return ${module}.bind(this) }\n` ;
 
     const data = fs.readFileSync(`./lib/${module}.js`, 'UTF-8');
@@ -22,13 +44,48 @@ fs.readdirSync(__dirname+'/lib').forEach(modules => {
     });
 
 });
+classBuild += 'info() {\n' +
+    '        let arMethods = sourceLib;\n' +
+    '        return new Proxy(arMethods,{\n' +
+    '                get: function(obj, prop) {\n' +
+    '                    let result, categories = {};\n' +
+    '                    if (prop in obj) { return obj[prop] }\n' +
+    '                    if (prop === \'number\') { return obj.length }\n' +
+    '                    for (let product of obj) {\n' +
+    '                        if (product.name === prop) { result = product }\n' +
+    '                        if (categories[product.category]) { categories[product.category].push(product) } \n' +
+    '                        else { categories[product.category] = [product]}\n' +
+    '                    }\n' +
+    '                    if (result) { return result }\n' +
+    '                    if (prop === \'names\') { return Object.keys(obj).map(items=>obj[items].name).filter(Boolean) }\n' +
+    '                    if (prop in categories) { return categories[prop] }\n' +
+    '                    if (prop === \'categories\') { return Object.keys(categories) }\n' +
+    '                    return undefined;\n' +
+    '                }\n' +
+    '            });\n' +
+    '    }\n';
+classBuild += '}\n';
 
-classBuild += '}';
 
+
+console.log(header(pjson.version, new Date()));
 console.log(importMod);
-console.log('========================\n');
+console.log('const sourceLib \t\t = '+JSON.stringify(arMethods)+'\n');
+console.log(params);
 console.log(classBuild);
-console.log('========================\n');
-console.log(arMethods);
+console.log('module.exports = jsPackTools;\n');
 
+let index = '';
+index += header(pjson.version, new Date());
+index += importMod;
+index += 'const sourceLib \t\t = '+JSON.stringify(arMethods)+';\n';
+index += params;
+index += classBuild;
+index += 'module.exports = jsPackTools;\n';
 
+fs.writeFile("./index.js", index, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("Generate index.js!");
+});
--- index.js
+++ index.js
@@ -2,30 +2,31 @@
  * JS-PackTools. Is a little toolBox for developer specifically with webScraping and format date and text
  * @module JS-PackTools | API Doc
  * @author Jesús A. Pérez S.
+ * {@version 1.0.1 | last Update: Sat Nov 02 2019 23:05:43 GMT-0500 (GMT-05:00) 
  * {@link https://github.com/jasp402/js-packtools}
  */
-const constant          = require(__dirname+'/constants');
-const assert            = require('assert');
-const clearFolder       = require('./lib/clearFolder');
-const createFolders     = require('./lib/createFolders');
-const csvToJson         = require('./lib/csvToJson');
-const customDate        = require('./lib/customDate');
-const deleteFile        = require('./lib/deleteFile');
-const differenceDay     = require('./lib/differenceDay');
-const everyOrNone       = require('./lib/everyOrNone');
-const formatSeconds     = require('./lib/formatSeconds');
-const generateRageDate  = require('./lib/generateRageDate');
-const getFinalPath      = require('./lib/getFinalPath');
-const groupBy           = require('./lib/groupBy');
-const increaseDays      = require('./lib/increaseDays');
-const objectToDate      = require('./lib/objectToDate');
-const parseDate         = require('./lib/parseDate');
-const validateDir       = require('./lib/validateDir');
-const validateYear      = require('./lib/validateYear');
-const writeLog          = require('./lib/writeLog');
-const writeLogError     = require('./lib/writeLogError');
-const _log              = require('./lib/_log');
-
+const constant 			 = require(__dirname+'/constants');
+const assert 			 = require('assert');
+const clearFolder 		 = require('./lib/clearFolder');
+const createFolders 	 = require('./lib/createFolders');
+const csvToJson 		 = require('./lib/csvToJson');
+const customDate 		 = require('./lib/customDate');
+const deleteFile 		 = require('./lib/deleteFile');
+const differenceDay 	 = require('./lib/differenceDay');
+const everyOrNone 		 = require('./lib/everyOrNone');
+const formatSeconds 	 = require('./lib/formatSeconds');
+const generateRageDate 	 = require('./lib/generateRageDate');
+const getFinalPath 		 = require('./lib/getFinalPath');
+const groupBy 			 = require('./lib/groupBy');
+const increaseDays 		 = require('./lib/increaseDays');
+const objectToDate 		 = require('./lib/objectToDate');
+const parseDate 		 = require('./lib/parseDate');
+const validateDir 		 = require('./lib/validateDir');
+const validateYear 		 = require('./lib/validateYear');
+const writeLog 			 = require('./lib/writeLog');
+const writeLogError 	 = require('./lib/writeLogError');
+const _log 				 = require('./lib/_log');
+const sourceLib 		 = [{"name":"clearFolders","category":"Path/Files","description":"This simply clear the content a folder.","version":"1.0.0"},{"name":"createFolders","category":"Path/Files","description":"This create a folder whit sub-folder of date by default if param `withDate` is True.","version":"1.0.0"},{"name":"csvToJson","category":"convert","description":"This read a document CSV and convert in a Object Javascript (JSON).","version":"1.0.0"},{"name":"customDate","category":"Time/Date","description":"It's a date control. Without parameters give the current date, use the parameters to customize its functionality.","version":"1.0.0"},{"name":"deleteFile","category":"Path/Files","description":"Check if the file exists before deleting","version":"1.0.0"},{"name":"differenceDay","category":"Time/Date","description":"This simply clear the content a folder.","version":"1.0.0"},{"name":"everyOrNone","category":"Arrays/Object","description":"This snippet returns true if the predicate function returns true for all elements in a collection and false otherwise. \\n You can omit the second argument fn if you want to use Boolean as a default value.","version":"1.0.0"},{"name":"formatSeconds","category":"Time/Date","description":"This only format a value float","version":"1.0.0"},{"name":"generateRageDate","category":"Time/Date","description":"This function allows you to separate a given date in the number of days assigned to it.","version":"1.0.0"},{"name":"getFinalPath","category":"Path/Files","description":"Create structure of folders with parameters in constructor.","version":"1.0.0"},{"name":"groupBy","category":"Arrays/Object","description":"Sort the object by placing the value of the assigned property as key","version":"1.0.0"},{"name":"increaseDays","category":"Time/Date","description":"Add days to date, can defined a maxDate.","version":"1.0.0"},{"name":"objectToDate","category":"Time/Date","description":"Generate a object with date details.","version":"1.0.0"},{"name":"parseDate","category":"Time/Date","description":"Using a string and a format transform the string in date.","version":"1.0.0"},{"name":"validateDir","category":"Path/Files","description":"Validate if there is a route. if not, create this route.","version":"1.0.0"},{"name":"validateYear","category":"Time/Date","description":"Can be current year or spend the year to validate","version":"1.0.0"},{"name":"writeLog","category":"global","description":"write file .log in folder default of class.","version":"1.0.0"},{"name":"writeLogError","category":"global","description":"write file .log in folder default of class.","version":"1.0.0"},{"name":"log","category":"global","description":"It integrates the functions registry of errors and registry of execution for the document of registry in his project.","version":"1.0.0"}];
 class parameters {
     constructor(config={}){
         assert.ok(typeof config === 'object', constant.ERROR_CONSTRUCTOR);
@@ -38,159 +39,42 @@ class parameters {
         this.logFormatDate      = ("logFormatDate"     in config) ? config.logFormatDate            : 'yyyy-mm-dd ~h:m:i';  //=> yyyy-mm-dd | hh:mm:sss
     }
 }
-class jsPackTools extends parameters {
-    get clearFolder() { return clearFolder.bind(this) }
-    get createFolders() { return createFolders.bind(this) }
-    get csvToJson() { return csvToJson.bind(this) }
-    get customDate() { return customDate.bind(this) }
-    get deleteFile() { return deleteFile.bind(this) }
-    get differenceDay() { return differenceDay.bind(this) }
-    get everyOrNone() { return everyOrNone.bind(this) }
-    get formatSeconds() { return formatSeconds.bind(this) }
-    get generateRageDate() { return generateRageDate.bind(this) }
-    get getFinalPath() { return getFinalPath.bind(this) }
-    get groupBy() { return groupBy.bind(this) }
-    get increaseDays() { return increaseDays.bind(this) }
-    get objectToDate() { return objectToDate.bind(this) }
-    get parseDate() { return parseDate.bind(this) }
-    get validateDir() { return validateDir.bind(this) }
-    get validateYear() { return validateYear.bind(this) }
-    get writeLog() { return writeLog.bind(this) }
-    get writeLogError() { return writeLogError.bind(this) }
-    get _log() { return _log.bind(this) }
-    info() {
-        let arMethods = [ { name: 'clearFolders',
-            category: 'Path/Files',
-            description: 'This simply clear the content a folder.',
-            version: '1.0.0' },
-            { name: 'createFolders',
-                category: 'Path/Files',
-                description:
-                    'This create a folder whit sub-folder of date by default if param `withDate` is True.',
-                version: '1.0.0' },
-            { name: 'csvToJson',
-                category: 'convert',
-                description:
-                    'This read a document CSV and convert in a Object Javascript (JSON).',
-                version: '1.0.0' },
-            { name: 'customDate',
-                category: 'Time/Date',
-                description:
-                    'It\'s a date control. Without parameters give the current date, use the parameters to customize its functionality.',
-                version: '1.0.0' },
-            { name: 'deleteFile',
-                category: 'Path/Files',
-                description: 'Check if the file exists before deleting',
-                version: '1.0.0' },
-            { name: 'differenceDay',
-                category: 'Time/Date',
-                description: 'This simply clear the content a folder.',
-                version: '1.0.0' },
-            { name: 'everyOrNone',
-                category: 'Arrays/Object',
-                description:
-                    'This snippet returns true if the predicate function returns true for all elements in a collection and false otherwise. \\n You can omit the second argument fn if you want to use Boolean as a default value.',
-                version: '1.0.0' },
-            { name: 'formatSeconds',
-                category: 'Time/Date',
-                description: 'This only format a value float',
-                version: '1.0.0' },
-            { name: 'generateRageDate',
-                category: 'Time/Date',
-                description:
-                    'This function allows you to separate a given date in the number of days assigned to it.',
-                version: '1.0.0' },
-            { name: 'getFinalPath',
-                category: 'Path/Files',
-                description:
-                    'Create structure of folders with parameters in constructor.',
-                version: '1.0.0' },
-            { name: 'groupBy',
-                category: 'Arrays/Object',
-                description:
-                    'Sort the object by placing the value of the assigned property as key',
-                version: '1.0.0' },
-            { name: 'increaseDays',
-                category: 'Time/Date',
-                description: 'Add days to date, can defined a maxDate.',
-                version: '1.0.0' },
-            { name: 'objectToDate',
-                category: 'Time/Date',
-                description: 'Generate a object with date details.',
-                version: '1.0.0' },
-            { name: 'parseDate',
-                category: 'Time/Date',
-                description: 'Using a string and a format transform the string in date.',
-                version: '1.0.0' },
-            { name: 'validateDir',
-                category: 'Path/Files',
-                description: 'Validate if there is a route. if not, create this route.',
-                version: '1.0.0' },
-            { name: 'validateYear',
-                category: 'Time/Date',
-                description: 'Can be current year or spend the year to validate',
-                version: '1.0.0' },
-            { name: 'writeLog',
-                category: 'global',
-                description: 'write file .log in folder default of class.',
-                version: '1.0.0' },
-            { name: 'writeLogError',
-                category: 'global',
-                description: 'write file .log in folder default of class.',
-                version: '1.0.0' },
-            { name: 'log',
-                category: 'global',
-                description:
-                    'It integrates the functions registry of errors and registry of execution for the document of registry in his project.',
-                version: '1.0.0' } ];
+class jsPackTools extends parameters { 
+get clearFolder() { return clearFolder.bind(this) }
+get createFolders() { return createFolders.bind(this) }
+get csvToJson() { return csvToJson.bind(this) }
+get customDate() { return customDate.bind(this) }
+get deleteFile() { return deleteFile.bind(this) }
+get differenceDay() { return differenceDay.bind(this) }
+get everyOrNone() { return everyOrNone.bind(this) }
+get formatSeconds() { return formatSeconds.bind(this) }
+get generateRageDate() { return generateRageDate.bind(this) }
+get getFinalPath() { return getFinalPath.bind(this) }
+get groupBy() { return groupBy.bind(this) }
+get increaseDays() { return increaseDays.bind(this) }
+get objectToDate() { return objectToDate.bind(this) }
+get parseDate() { return parseDate.bind(this) }
+get validateDir() { return validateDir.bind(this) }
+get validateYear() { return validateYear.bind(this) }
+get writeLog() { return writeLog.bind(this) }
+get writeLogError() { return writeLogError.bind(this) }
+get _log() { return _log.bind(this) }
+info() {
+        let arMethods = sourceLib;
         return new Proxy(arMethods,{
                 get: function(obj, prop) {
-
-                    // The default behavior to return the value; prop is usually an integer
-                    if (prop in obj) {
-                        return obj[prop];
-                    }
-
-                    // Get the number of products; an alias of products.length
-                    if (prop === 'number') {
-                        return obj.length;
-                    }
-
                     let result, categories = {};
-
+                    if (prop in obj) { return obj[prop] }
+                    if (prop === 'number') { return obj.length }
                     for (let product of obj) {
-                        if (product.name === prop) {
-                            result = product;
-                        }
-
-
-                        if (categories[product.category]) {
-                            categories[product.category].push(product);
-                        } else {
-                            categories[product.category] = [product];
-                        }
+                        if (product.name === prop) { result = product }
+                        if (categories[product.category]) { categories[product.category].push(product) } 
+                        else { categories[product.category] = [product]}
                     }
-
-                    // Get a product by name
-                    if (result) {
-                        return result;
-                    }
-
-                    if (prop === 'names') {
-                        return Object.keys(obj).map(items=>obj[items].name).filter(Boolean);
-                    }
-
-                    // Get products by categories
-                    if (prop in categories) {
-                        return categories[prop];
-                    }
-
-                    // Get product categories
-                    if (prop === 'categories') {
-                        return Object.keys(categories);
-
-                    }
-
+                    if (result) { return result }
+                    if (prop === 'names') { return Object.keys(obj).map(items=>obj[items].name).filter(Boolean) }
+                    if (prop in categories) { return categories[prop] }
+                    if (prop === 'categories') { return Object.keys(categories) }
                     return undefined;
                 }
             });
