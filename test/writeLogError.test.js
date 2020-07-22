12217ebf8956619935f5678f78e1ca755a9fdd37
Jesús Antonio Pérez Sandoval <jasp402@gmail.com>
2020-07-22 06:54:32 -0500

    fix: deleted global.test.js is no required. - fix writeLogError.test.js remove folders autogenerated

--- test/writeLogError.test.js
+++ test/writeLogError.test.js
@@ -1,7 +1,18 @@
 const jsPackTools = require('../index')();
 const assert = require('assert');
+const fs = require('fs');
+
+const PATH = jsPackTools.getFinalPath();
+const FILE_LOG = 'logError.log';
 
 describe('SUITE - logError()', function () {
+
+    after('delete folders', () => {
+        fs.unlinkSync(`${PATH}/${FILE_LOG}`);
+        fs.rmdirSync(`${__dirname}/../${PATH.split('/').slice(0, -1).join('/')}`);
+        fs.rmdirSync(`${__dirname}/../${PATH.split('/')[0]}`);
+    });
+
     it('TEST # 1, Create throw exception', function () {
         assert.throws(
             () => {
