2c133fec73257b47e165541d8b14752feee54e33
Jesús Antonio Pérez Sandoval <jasp402@gmail.com>
2020-07-23 14:10:41 -0500

    fix: update index.js with new compiler

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * JS-PackTools. Is a little toolBox for developer specifically with webScraping and format date and text
  * @module JS-PackTools | API Doc
  * @author Jesús A. Pérez S.
- * {@version 1.9.5 | last Update: Wed Jul 22 2020 00:56:55 GMT-0500 (GMT-05:00) 
+ * {@version 1.9.5 | last Update: Thu Jul 23 2020 13:38:24 GMT-0500 (GMT-05:00)
  * {@link https://github.com/jasp402/js-packtools}
  */
 const constant 			 = require(__dirname+'/constants');
@@ -106,7 +106,7 @@ const sourceLib 		 = [
   "name": "formatSeconds",
   "category": "Time/Date",
   "description": "This only format a value float",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "test": null
  },
  {
@@ -202,18 +202,18 @@ const sourceLib 		 = [
  }
 ];
 class parameters {
-    constructor(config={}){
+    constructor(config = {})
+    {
         assert.ok(typeof config === 'object', constant.ERROR_CONSTRUCTOR);
-        this.constant           = constant;
-        this.folderName         = ("folderName"        in config) ? config.folderName                  : 'DATA';                 //=> data_audit, data_download, docs, pdfs, trash...
-        this.folderWithDate     = ("folderWithDate"    in config) ? Boolean(config.folderWithDate)     : true;                     //=> true => /2019_01_01/
-        this.folderFormatDate   = ("folderFormatDate"  in config) ? config.folderFormatDate            : 'yyyy~_~mm~_~dd';       //=> 'yyyy_mm_dd';
-        this.nameWriteLog       = ("nameWriteLog"      in config) ? config.nameWriteLog+'.log'       : 'logExecution.log';
-        this.nameWriteLogError  = ("nameWriteLogError" in config) ? config.nameWriteLogError+'.log'  : 'logError.log';
-        this.logFormatDate      = ("logFormatDate"     in config) ? config.logFormatDate               : 'yyyy-mm-dd ~h:m:i';    //=> yyyy-mm-dd | hh:mm:sss
+        this.constant          = constant;
+        this.folderName        = ("folderName" in config)           ? config.folderName                 : 'DATA';                 //=> data_audit, data_download, docs, pdfs, trash...
+        this.folderWithDate    = ("folderWithDate" in config)       ? Boolean(config.folderWithDate)    : true;                   //=> true => /2019_01_01/
+        this.folderFormatDate  = ("folderFormatDate" in config)     ? config.folderFormatDate           : 'yyyy~_~mm~_~dd';       //=> 'yyyy_mm_dd';
+        this.nameWriteLog      = ("nameWriteLog" in config)         ? config.nameWriteLog + '.log'      : 'logExecution.log';
+        this.nameWriteLogError = ("nameWriteLogError" in config)    ? config.nameWriteLogError + '.log' : 'logError.log';
+        this.logFormatDate     = ("logFormatDate" in config)        ? config.logFormatDate              : 'yyyy-mm-dd ~h:m:i';    //=> yyyy-mm-dd | hh:mm:sss
     }
-}
-class jsPackTools extends parameters { 
+}class jsPackTools extends parameters { 
 get allEqual() { return allEqual.bind(this) }
 get capitalLetter() { return capitalLetter.bind(this) }
 get clearFolder() { return clearFolder.bind(this) }
@@ -238,26 +238,44 @@ get validateYear() { return validateYear.bind(this) }
 get writeLog() { return writeLog.bind(this) }
 get writeLogError() { return writeLogError.bind(this) }
 get _log() { return _log.bind(this) }
-info() {
-        let arMethods = sourceLib;
-        return new Proxy(arMethods,{
-                get: function(obj, prop) {
-                    let result, categories = {};
-                    if (prop in obj) { return obj[prop] }
-                    if (prop === 'number') { return obj.length }
-                    for (let product of obj) {
-                        if (product.name === prop) { result = product }
-                        if (categories[product.category]) { categories[product.category].push(product) } 
-                        else { categories[product.category] = [product]}
-                    }
-                    if (result) { return result }
-                    if (prop === 'names') { return Object.keys(obj).map(items=>obj[items].name).filter(Boolean) }
-                    if (prop === 'tests') { return Object.keys(obj).map(items=>obj[items].test).filter(Boolean) }
-                    if (prop in categories) { return categories[prop] }
-                    if (prop === 'categories') { return Object.keys(categories) }
-                    return undefined;
+ info() {
+    let arMethods = sourceLib;
+    return new Proxy(arMethods, {
+        get: function (obj, prop) {
+            let result, categories = {};
+            if (prop in obj) {
+                return obj[prop]
+            }
+            if (prop === 'number') {
+                return obj.length
+            }
+            for (let product of obj) {
+                if (product.name === prop) {
+                    result = product
                 }
-            });
-    }
-}
+                if (categories[product.category]) {
+                    categories[product.category].push(product)
+                } else {
+                    categories[product.category] = [product]
+                }
+            }
+            if (result) {
+                return result
+            }
+            if (prop === 'names') {
+                return Object.keys(obj).map(items => obj[items].name).filter(Boolean)
+            }
+            if (prop === 'tests') {
+                return Object.keys(obj).map(items => obj[items].test).filter(Boolean)
+            }
+            if (prop in categories) {
+                return categories[prop]
+            }
+            if (prop === 'categories') {
+                return Object.keys(categories)
+            }
+            return undefined;
+        }
+    });
+}}
 module.exports = (params) => new jsPackTools(params);
